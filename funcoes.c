0501bd78881397a0801588876f0024825c602257
diogofonseca01 <128496536+diogofonseca01@users.noreply.github.com>
2023-03-29 18:42:21 +0000

    supermercado start

--- funcoes.c
+++ funcoes.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "structs.c"
 
+#include "structs.c"
 #include "funcoes.h"
 
 
-SUPERMERCADO CriarSM(SUPERMERCADO *SM, int nCaixas,time_t horaAbertura,time_t horaFecho,int nmrMinCliFechaCaixa,int nmrMaxClientesFila){
-    
-}
+
---
+++ supermercado.c
@@ -0,0 +1,11 @@
+#include "structs.c"
+#include "supermercado.h"
+
+SUPERMERCADO CriarSM(){
+    int nCaixas, nmrMinCliFechaCaixa, nmrMaxClientesFila;
+    time_t horaAbertura, horaFecho;
+
+    SUPERMERCADO *SM = (SUPERMERCADO *)malloc(sizeof(SUPERMERCADO));
+
+    printf("")
+}
