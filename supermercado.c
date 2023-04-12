359d94a4a3eae823d55df3cba71818496c45990b
diogofonseca01 <128496536+diogofonseca01@users.noreply.github.com>
2023-04-12 18:27:22 +0000

    s

--- supermercado.c
+++ supermercado.c
@@ -9,11 +9,19 @@ SUPERMERCADO CriarSM(){
 
     SUPERMERCADO *SM = (SUPERMERCADO *)malloc(sizeof(SUPERMERCADO));
 
-    nCaixas = validarInt("Número de caixas do supermercado:",1,10);
-    nmrMaxClientesFila = validarInt("Máximo de clientes na fila:",1,20);
-    nmrMinCliFechaCaixa = validarInt("Número de clientes para fechar a caixa:",1,5);
+    //nome = getString("Nome do Supermercado:");
 
+    horaAbertura; 
+    horaFecho;
 
+    nCaixas = validarInt("\nNúmero de caixas do supermercado:",1,10);
+    nmrMaxClientesFila = validarInt("\nMáximo de clientes na fila:",1,20);
+    nmrMinCliFechaCaixa = validarInt("\nNúmero de clientes para fechar a caixa:",1,5);
+
+    
+    ListaGenerica * ListaCaixas = CriarLG();
+
+    SM->nome = nome;
     SM->numCaixas = nCaixas;
     SM->nmrMaxClientesFila = nmrMaxClientesFila;
     SM->nmrMinCliFechaCaixa = nmrMinCliFechaCaixa;
