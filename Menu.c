95f25d3688390da7c2e5926387b98cca72294450
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2023-04-18 21:12:37 +0100

    Update Menu.c

--- Menu.c
+++ Menu.c
@@ -1,2 +1,10 @@
 #include "menu.h"
-
+ 
+void menuPrincipal(){
+  puts("====================================");
+  puts("=\t\tMenu Principal=");
+  puts("= 1 - Iniciar o Programa=");
+  puts("==");
+  puts("==");
+  puts("=  - Terminar o programa=");
+}
