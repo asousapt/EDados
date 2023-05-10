d518345d11d03ab59cc70454ab93759bb5e2227a
diogofonseca01 <128496536+diogofonseca01@users.noreply.github.com>
2023-05-10 19:16:18 +0000

    Relogio e menus

---
+++ Relogio.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "Relogio.h"
+
+void StartRelogio(Relogio *R, int Vel, SUPERMERCADO *S){
+    R->START = time(0);
+    R->VELOCIDADE = Vel;
+
+    struct tm *tmp = localtime(&R->START);
+    sscanf(S->horaAbertura, "%d:%d", &tmp->tm_hour, &tmp->tm_min);
+    R->Hora_Inicio = mktime(tmp);
+    printf("Hora de Arranque = [%s]\n", asctime(localtime(&(R->Hora_Inicio))));
+}
+
+time_t VerTimeRelogio(Relogio *R){
+    time_t Dif = difftime(time(0), R->START);
+    time_t Simulada = R->Hora_Inicio + Dif * R->VELOCIDADE;
+    return Simulada;
+}
+
+void WaitSegundos(int s){
+    clock_t T0 = clock();
+    clock_t T1 = T0 + s;
+    while (clock() < T1);
+}
+
+void Wait(int s){
+    WaitSegundos(s*CLOCKS_PER_SEC);
+}
+
+//Verificar tecla pressionada
+// if (kbhit){
+//     ch = getch();
+//     //verificar se e o enter
+// }
+
+//Funcao Loop
+// int Run(Supermercado *S)
+// {
+//     while(1)
+//     {
+//         printf("Estou a trabalhar... entre as [%d] e as [%d]\n", S->HORA_INICIO, S->HORA_FIM);
+//         size_t MEMORIA = MemoriaOcupadaSupermercado(S);
+//         printf("MEMORIA do [%s]= %lu Bytes\n", S->NOME, MEMORIA);
+//         // ShowProdutos(LIDL);
+//         // ShowClientes(LIDL);
+//         // ShowFuncionarios(S);
+//         ShowEstado(S);
+
+//         ProcessarCaixas(S);
+
+//         EntrarMaisCliente(S);
+
+//         IrCaixaPagar(S);
+
+//         wait(2000);
+//     }
+// }
---
+++ Relogio.h
@@ -0,0 +1,21 @@
+#ifndef RELOGIO_H_INCLUDED
+#define RELOGIO_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "supermercado.h"
+
+typedef struct{
+    time_t START;
+    int VELOCIDADE;
+    time_t Hora_Inicio;
+}Relogio;
+
+void StartRelogio(Relogio *R, int Vel, SUPERMERCADO *S);
+time_t VerTimeRelogio(Relogio *R);
+void WaitSegundos(int s);
+void Wait(int s);
+
+#endif // RELOGIO_H_INCLUDED
