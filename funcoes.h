9098327cbdfd10f4596b29a35587363fab21fa82
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2023-05-30 20:54:04 +0000

    string

--- funcoes.h
+++ funcoes.h
@@ -7,7 +7,7 @@
 #include <time.h>
 
 int validarInt(char *pergunta, int menor,int maior);
-char *getFrase(char *pergunta);
+char *getString(char *pergunta);
 int aleatorio(int min, int max);
 int validaHoras(int hora, int minutos);
 int validaHorasFecho(int hora, int minutos,time_t abertura);
