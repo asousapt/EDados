3a7fb689b784fd5b3cbb933a624a761e6976f7c2
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2023-05-31 21:52:36 +0000

    changes

--- Menu.c
+++ Menu.c
@@ -7,20 +7,10 @@ void menuUtilizador(){
   printf("====================================");
   printf("= 1 - Operacoes com Caixas         =");
   printf("= 2 - Operacoes com Clientes       =");
-  printf("= 3 - Operacoes com Clientes       =");
-  printf("= 5 - Continuar                    =");
+  printf("= 3 - Estatistica                  =");
+  printf("= 4 - Continuar                    =");
   printf("= 0 - Terminar o programa          =");
   printf("====================================");
-}
-
-void menuMostrarClientes(){
-  printf("============================================");
-  printf("============ Mostrar Clientes ==============");
-  printf("============================================");
-  printf("= 1 - Mostrar Clientes atendidos numa caixa=");
-  printf("= 2 - Pesquisar Cliente                    =");
-  printf("= 0 - Sair                                 =");
-  printf("============================================");
 }
 
 void menuOperacoesClientes(){
@@ -38,26 +28,12 @@ void menuOperacoesCaixa(){
   printf("============================================");
   printf("========== Operacoes com Caixas ============");
   printf("============================================");
+  printf("= 1 - Mostrar Todas as Caixas              =");
+  printf("= 2 - Mostrar Caixas Abertas               =");
+  printf("= 3 - Mostrar Caixas Fechadas              =");
   printf("= 1 - Mostrar Clientes atendidos numa caixa=");
-  printf("= 3 - Mostrar Caixas                       =");
   printf("= 0 - Sair                                 =");
   printf("============================================");
-}
-
-void menuMostrarClientes(){
-  int opcao = validarInt("Escolha uma das opcoes: ", 0,3);
-}
-
-void menuMostrarCaixa(){
-  
-  printf("====================================");
-  printf("========== Mostrar Caixas ==========");
-  printf("====================================");
-  printf("= 1 - Todas as Caixas              =");
-  printf("= 2 - Caixas Abertas               =");
-  printf("= 3 - Caixas Fechadas              =");
-  printf("= 0 - Sair                         =");
-  printf("====================================");
 }
 
 
--- Switch.h
+++ Switch.h
@@ -1,3 +1,7 @@
+#ifndef SWITCH_H
+#define SWITCH_H
 #include "Menu.h"
+#include "funcoes.h"
 
 void switchPrincipal();
+#endif
